33667c991cd5f76686550bd5cb8803e83cc8610f
greengrass100 <anku10kumar@gmail.com>
2019-06-09 17:39:11 -0500

    checkpoint added

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,24 +1,29 @@
 const MenuController = require("../controllers/MenuController");
 
- describe("MenuController", () => {
+describe("MenuController", () => {
 
-   beforeEach(() => {
-      this.menu = new MenuController();
+  beforeEach(() => {
+    this.menu = new MenuController();
+  });
+
+  describe("#getContactCount()", () => {
+
+
+    it("should return 0 when no contacts are in the book", () => {
+      expect(this.menu.getContactCount()).toBe(0);
     });
 
-   describe("#getContactCount()", () => {
-
-
-     it("should return 0 when no contacts are in the book", () => {
-
-       expect(this.menu.getContactCount()).toBe(0);
-     });
-
-     it("should return 1 when there is exactly one contact in the book", () => {
-
-              this.menu.contacts.push("Bob");
+    it("should return 1 when there is exactly one contact in the book", () => {
+      this.menu.contacts.push("Bob");
       expect(this.menu.getContactCount()).toBe(1)
     });
 
-   });
- });
+  });
+
+  describe("#remindMe()", () =>{
+
+    it("should return string containing Learning is a life-long pursuit", () =>{
+      expect (this.menu.remindMe()).toBe("Learning is a life-long pursuit")
+    })
+  });
+});
